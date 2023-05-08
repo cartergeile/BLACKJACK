2c8c123c81ba403aa654657f32dd81e023918ebe
Carter Geile <cartergeile@Carters-MacBook.local>
2023-05-08 18:36:47 -0500

    just missing a little functionality but almost done

--- script.js
+++ script.js
@@ -46,12 +46,16 @@
       wager += parseInt(money, 0);
     }
 
+    this.setWager = function(){
+      wager += parseInt(money, 0);
+    }
+
     this.resetWager = function(){
       wager = 0
     }
 
     this.checkWager = function(){
-      return wager <= cahs ? true : false;
+      return wager <= cash ? true : false;
     }
 
     this.getCash = function(){
@@ -64,7 +68,7 @@
     }
 
     this.getBank = function(){
-      $('#bank').html('Winnings : $' +bank.formatMoney(2, '.', ','));
+      $('#bank').html('Winnings : $' + bank.formatMoney(2, '.', ','));
       if(bank < 0){
         $('#bank').html(`Winnings : <span>-$${bank.formatMoney(2, '.', ',').toString().replace('-', '')}</span>`)
       }
@@ -226,12 +230,169 @@
     }
   }
 
+  function resetBoard(){
+    $('#dhand').html('');
+    $('#phand').html('');
+    $('#result').html('');
+    $('#phand, #dhand').css('left', 0);
+  }
+
+  function showBoard(){
+    deal.dealCard(4, 0 [player, dealer, player, dealer])
+  }
+
+  function showAlert(msg){
+    $('#alert span').html(`<strong>${msg}</strong>`)
+    $('#alert').fadeIn();
+  }
+
+  function setActions(opts){
+    let hand = player.getHand();
+
+    if(!running){
+      $('#deal').prop('disabled', false);
+      $('#hit').prop('disabled', true);
+      $('#stand').prop('disabled', true);
+      $('#double').prop('disabled', true);
+      $('#split').prop('disabled', true);
+      $('#insurance').prop('disabled', true);
+    }
+
+    if(opts === 'run'){
+      $('#deal').prop('disabled', true);
+      $('#hit').prop('disabled', false);
+      $('#stand').prop('disabled', false);
+
+      if(player.checkWager(wager * 2)){
+        $('#double').prop('disabled', false);
+      }
+    }else if (opts === 'split'){
+      $('#split').prop('disabled', false);
+    }else if (opts === 'insurance'){
+      $('#insurance').prop('disabled', false);
+    }else if(hand.length > 2){
+      $('#double').prop('disabled', true);
+      $('#split').prop('disabled', true);
+      $('#insurance').prop('disabled', true);
+    }
+  }
+
   function Game(){
     this.newGame = function(){
       let wager = $.trim($('#wager').val());
 
+      player.resetWager();
+      player.setWager(wager);
+
+      if(player.checkWager()){
+        $('#deal').pop('disabled', true);
+        resetBoard();
+        player.setCash(-wager);
+
+        deal = new Deal();
+        running = true;
+        blackjack = false;
+        insured = false;
+
+        player.resetHand();
+        dealer.resetHand();
+        showBoard();
+      }else{
+        player.setWager(-wager);
+        $('#alert').removeClass('alert-info alert-success').addClass('alert-error');
+        showAlert('Wager cannot exceed available cash')
+      }
 
     }
   }
 
+  function renderCard(ele, sender, type, item){
+    let hand, i, card;
+
+    if(!item){
+      hand = sender.getHand();
+      i = hand.length - 1;
+      card = new Card(hand[i]);
+    }else {
+      hand = dealer.getHand();
+      card= new Card(hand[1])
+    }
+
+    let rank = card.getRank(),
+        suit = card.getSuit(),
+        color = 'red',
+        posx = 402,
+        posy = 182,
+        speed = 200,
+        cards = ele + ' .card=' + i;
+
+    if(i > 0){
+      posx -= 50 * i;
+    }
+
+    if(!item){
+      $(ele).append(
+        `<div class='card-${i} ${type}'>
+          <span class='pos-0'>
+            <span class='rank'>&nbsp;</span>
+            <span class='suit'>&nbsp;</span>
+          </span>
+          <span class='pos-1'>
+            <span class='rank'>&nbsp;</span>
+            <span class='suit'>&nbsp;</span>
+          </span>
+        </div>`
+      )
+    }
+  }
+
+
+  $('#deal').on('cllick', function(){
+    let cash = parseInt(player.getCash());
+
+    $('#alert').fadeOut();
+
+    if(cash > 0 && !running){
+      if($.trim($('#wager').val()) > 0){
+        game.newGame();
+      }else{
+        $('#alert').removeClass('alert-info alert-success').addClass('alert-error');
+        SpeechRecognitionAlternative('The minimum bet is $1.')
+      }
+    }else{
+      $('#myModal').modal();
+    }
+  });
+
+  $('#hit').on('click', function(){
+    player.hit()
+  })
+
+  $('#stand').on('click', function(){
+    player.stand()
+  })
+
+  $('#double').on('click', function(){
+    player.double()
+  })
+
+  $('#split').on('click', function(){
+    player.split()
+  })
+
+  $('#insurance').on('click', function(){
+    player.insurance()
+  })
+
+  $('#wager').numOnly();
+  $('#actions:not(#wager), #game, #myModal').disableSelection();
+  $('#newGame, #cancel').on('click', function(e){
+    e.preventDefualt();
+  });
+  $('#cancel').on('click', function(){
+    $('#myModal').modal('hide');
+  })
+  $('#wager').val(100);
+  $('#cash span').html(player.getCash());
+  player.getBank();
 });
